4567119db1cbc5155c2b8a1d30c2c96d26d5c891
wilson_wp <842104779@qq.com>
2019-08-15 11:05:44 +0800

    删除swiper配置

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -45,19 +45,8 @@
       </el-carousel-item>
     </el-carousel>
 
-    <!--<div style="position:relative">-->
-      <!--<swiper :options="swiperOption">-->
-        <!--<swiper-slide><img class="s-img" src="../assets/4.jpg"></swiper-slide>-->
-        <!--<swiper-slide><img class="s-img" src="../assets/2.jpg"></swiper-slide>-->
-        <!--<swiper-slide><img class="s-img" src="../assets/3.jpg"></swiper-slide>-->
-        <!--<swiper-slide><img class="s-img" src="../assets/5.jpg"></swiper-slide>-->
-        <!--<swiper-slide><img class="s-img" src="../assets/6.jpg"></swiper-slide>-->
-      <!--</swiper>-->
-      <!--<div class="swiper-scrollbar"></div>-->
-      <!--<div class="swiper-button-next"></div>-->
-      <!--<div class="swiper-button-prev"></div>-->
-      <!--<div class="swiper-pagination"></div>-->
-    <!--</div>-->
+
+
 
   </div>
 </template>
@@ -73,43 +62,6 @@ export default {
     return {
       defaultActive: '1',
       search: '',
-      swiperOption: {
-        speed: 500,
-        autoplay: {
-          delay: 2000, //自动切换的时间间隔，单位ms
-          stopOnLastSlide: false, //当切换到最后一个slide时停止自动切换
-          disableOnInteraction: true, //用户操作swiper之后，是否禁止autoplay。
-          reverseDirection: false, //开启反向自动轮播。
-          waitForTransition: false, //等待过渡完毕。自动切换会在slide过渡完毕后才开始计时。
-        },
-        navigation: {
-          nextEl: '.swiper-button-next', //前进按钮的css选择器或HTML元素。
-          prevEl: '.swiper-button-prev', //后退按钮的css选择器或HTML元素。
-          hideOnClick: true, //点击slide时显示/隐藏按钮
-          disabledClass: 'my-button-disabled', //前进后退按钮不可用时的类名。
-          hiddenClass: 'my-button-hidden', //按钮隐藏时的Class
-        },
-        pagination: {
-          el: '.swiper-pagination',
-          type: 'bullets',// fraction
-          //type: 'fraction',
-          //type : 'progressbar',
-          //type : 'custom',
-          progressbarOpposite: true, //使进度条分页器与Swiper的direction参数相反
-          bulletElement : 'li', //设定分页器指示器（小点）的HTML标签。
-          dynamicBullets: true,  //动态分页器，当你的slide很多时，开启后，分页器小点的数量会部分隐藏。
-          dynamicMainBullets: 2, //动态分页器的主指示点的数量
-          hideOnClick: true, //默认分页器会一直显示。这个选项设置为true时点击Swiper会隐藏/显示分页器。
-          clickable: true, //此参数设置为true时，点击分页器的指示点分页器会控制Swiper切换。
-        },
-        scrollbar: {
-          el: '.swiper-scrollbar',
-          hide: false, //滚动条是否自动隐藏。默认：false，不会自动隐藏。
-          draggable: true, //该参数设置为true时允许拖动滚动条。
-          snapOnRelease: true, //如果设置为false，释放滚动条时slide不会自动贴合。
-          dragSize: 30, //设置滚动条指示的尺寸。默认'auto': 自动，或者设置num(px)。
-        },
-      },
     }
   }
 };
